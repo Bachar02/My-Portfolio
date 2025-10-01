62293fdb34e91abe16022e2b7b32fb0d707ceb22
Bachar02 <bouguerrabachar2@gmail.com>
2025-10-01 18:47:12 +0100

    awards

--- src/components/Awards/Awards.js
+++ src/components/Awards/Awards.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
-import Particle from "../Particle"; 
+import Particle from "../Particle";
 import ProjectCard from "../Projects/ProjectCards.js";
 import awardImage1 from "../../Assets/Projects/car-finder.png";
 
@@ -8,47 +8,47 @@ function Awards() {
   const awards = [
     {
       id: 1,
-      title: "Wabna Hackathon Winner",
+      title: "IEEE CS INSAT  Orbit 1.0 Ideathon– 2nd place",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        "Awarded for developing EcoAI-Train: an AI sustainability platform that tracks and reduces energy waste during model training. The project focused on energy-efficient scheduling and carbon footprint reduction of large AI models.",
       imageSrc: awardImage1,
-      year: "2023",
+      year: "2025",
       ghLink: "https://github.com/Bachar02/PFA",
       demoLink: "https://youtu.be/dQw4w9WgXcQ"
     },
     {
       id: 2,
-      title: "9o7b Hackathon Winner",
+      title: "IEEE R8 Industrial Bootcamp– 1st place",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-      imageSrc: awardImage1,
-      year: "2022",
+        "Developed a sustainable agriculture solution combining aeroponics, bioengineered microbial nutrients, and AI- powered monitoring.Integrated real - time IoT sensors, ultrasonic pest control, and computer vision for fruit detection and maturity analysis.Achieved a projected ROI of 128.57 %.",
+  imageSrc: awardImage1,
+    year: "2025",
       ghLink: "https://github.com/Bachar02/PFA",
-      demoLink: "https://youtu.be/dQw4w9WgXcQ" 
-    },
-    {
-      id: 3,
-      title: "Asghir zib Hackathon Winner",
+        demoLink: "https://youtu.be/dQw4w9WgXcQ"
+},
+{
+  id: 3,
+    title: "NRTF Hackathon– 1st place",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-      imageSrc: awardImage1,
-      year: "2021",
-      ghLink: "https://github.com/Bachar02/PFA",
-      demoLink: "https://youtu.be/dQw4w9WgXcQ"
-    },
+  "Energy innovation integrating mobile solar panels and an AI-controlled turbine, designed to meet the energy needs of non-electrified urban areas, providing reliable power supply.",
+    imageSrc: awardImage1,
+      year: "2024",
+        ghLink: "https://github.com/Bachar02/PFA",
+          demoLink: "https://youtu.be/dQw4w9WgXcQ"
+},
   ];
 
-  return (
-    <Container fluid className="awards-section">
-      <Particle /> 
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Awards</strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are some of the hackathons and competitions I've won.
-        </p>
-        {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+return (
+  <Container fluid className="awards-section">
+    <Particle />
+    <Container>
+      <h1 className="project-heading">
+        My Recent <strong className="purple">Awards</strong>
+      </h1>
+      <p style={{ color: "white" }}>
+        Here are some of the hackathons and competitions I've won.
+      </p>
+      {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           {awards.map((award) => (
             <Col md={4} className="award-card" key={award.id}>
               <Card className="award-card-container" style={{ width: "18rem" }}>
@@ -67,24 +67,24 @@ function Awards() {
             </Col>
           ))}
         </Row> */}
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          {awards.map((award) => (
-            <Col md={4} className="project-card" key={award.id}>
-              <ProjectCard
-                imgPath={award.imageSrc}
-                isBlog={false}
-                title={award.title}
-                description={award.description}
-                // ghLink={award.ghLink}
-                // demoLink={award.demoLink}
-                year = {award.year}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        {awards.map((award) => (
+          <Col md={4} className="project-card" key={award.id}>
+            <ProjectCard
+              imgPath={award.imageSrc}
+              isBlog={false}
+              title={award.title}
+              description={award.description}
+              // ghLink={award.ghLink}
+              // demoLink={award.demoLink}
+              year={award.year}
+            />
+          </Col>
+        ))}
+      </Row>
     </Container>
-  );
+  </Container>
+);
 }
 
 export default Awards;
